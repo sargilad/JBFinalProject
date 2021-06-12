657ec8c7710852589abec43910345ec3e50383ca
sargilad <sargilad@gmail.com>
2021-06-12 09:27:14 +0300

    api crud tests created

--- open_project/tests/api_tests/tests/crud_tests.py
+++ open_project/tests/api_tests/tests/crud_tests.py
@@ -1,5 +1,5 @@
 import configparser
-import time
+import json
 from http import HTTPStatus
 
 from open_project.tests.api_tests.rest.app_requests import RestRequests
@@ -13,73 +13,90 @@ questions_sections_list = config_parser.sections()
 domain = config_parser['env']['domain']
 api_token = config_parser['user']['api_token']
 
+entities = OpenProjectEntities()
+rest_requests = RestRequests(domain, api_token)
 
-def main():
-    entities = OpenProjectEntities()
-    rest_requests = RestRequests(domain, api_token)
 
+def test_create_project() -> json:
     # create project
     name = CommonUtilities.get_random_string(prefix="proj_")
     description = "This is the first test project"
     body = entities.get_project_create_body(project_name=name, description=description)
     project = rest_requests.create_project(body)
-    # Assert
-    # project['name'] == name
-    # project['identifier'] == name
+    assert project['name'] == name
+    # assert project['identifier'] == name #todo fix
 
-    # Get project
-    tic = time.perf_counter()
+    return project
+
+
+def test_get_project():
+    project = test_create_project()
+    name = project['name']
+    description = project['description']['raw']
+
     project = rest_requests.get_single_project(id=project['id'])
-    toc = time.perf_counter()
-    # Assert
-    # project['name'] == name
-    # project['description']['raw'] == description)
+    assert project['name'] == name
+    assert project['description']['raw'] == description
 
-    # Update project
-    description = "This is a project description"
+
+def test_update_project():
+    project = test_create_project()
+
+    description = "Updated description"
     body = entities.get_project_update_body(description=description)
     project = rest_requests.update_project(project['id'], body)
-    # Assert
-    # project['description']['raw'] == description)
+    assert project['description']['raw'] == description
 
-    # Delete project
+
+def test_delete_project():
+    project = test_create_project()
+
     status = rest_requests.delete_project(id=project['id'], body={})
-    # Check status == HTTPStatus.NO_CONTENT
+    assert status == HTTPStatus.NO_CONTENT
 
     project = rest_requests.get_single_project(id=project['id'], expected_status=HTTPStatus.NOT_FOUND, attempts=5)
-    # Assert project = {}
+    assert project == {}
 
-    # Create work package
-    body = entities.get_project_create_body(project_name=CommonUtilities.get_random_string(prefix="proj_"))
-    project = rest_requests.create_project(body)
+
+def test_create_work_package() -> json:
+    project = test_create_project()
 
     pkg_name = CommonUtilities.get_random_string(prefix="pkg_")
     body = entities.get_create_work_package_body(pkg_name=pkg_name,
                                                  project_ref=project['_links']['self']['href'],
                                                  pkg_type="/api/v3/types/1")
     pkg = rest_requests.create_work_package(body=body)
-    # pkg['subject'] == name
+    assert pkg['subject'] == pkg_name
 
-    # GET work package
+    return pkg
+
+
+def test_get_work_package():
+    pkg = test_create_work_package()
+    pkg_name = pkg['subject']
+
     pkg = rest_requests.get_work_package(id=pkg['id'])
-    # task and package
-    # pkg['_links']['type']['title'] == 'Task'
-    # pkg['subject'] == pkg_name
+    assert pkg['_links']['type']['title'] == 'Task'
+    assert pkg['subject'] == pkg_name
 
-    # update work package
+
+def test_update_work_package():
+    pkg = test_create_work_package()
+
     lock_version = pkg['lockVersion']
-    package_description = "This is a package description"
+    package_description = "Package description updated"
     body = entities.get_work_package_update_body(lock_version=lock_version, description=package_description)
     pkg = rest_requests.update_work_package(id=pkg['id'], body=body)
-    # pkg['description']['raw'] == package_description
+    assert pkg['description']['raw'] == package_description
 
-    # delete work package
+
+def test_delete_work_package():
+    pkg = test_create_work_package()
+
     status = rest_requests.delete_work_package(id=pkg['id'])
-    # Check status == HTTPStatus.NO_CONTENT
+    assert status == HTTPStatus.NO_CONTENT
 
     pkg = rest_requests.get_work_package(id=pkg['id'])
-    # Assert pkg={}
+    assert pkg == {}
 
 
-if __name__ == '__main__':
-    main()
