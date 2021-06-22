c71ac17eae3fbe40133dac82e3b076c58d7d66b4
sargilad <sargilad@gmail.com>
2021-06-22 07:24:37 +0300

    add conf files as relative path to conf files

--- open_project/tests/api_tests/tests/test_api_base.py
+++ open_project/tests/api_tests/tests/test_api_base.py
@@ -1,16 +1,14 @@
 import configparser
-import os
 
 from open_project.tests.api_tests.rest.app_requests import RestRequests
 from open_project.tests.api_tests.rest.rest_entities import OpenProjectEntities
 from open_project.tests.api_tests.utilities.utilities import CommonUtilities
+from open_project.tests.env import conf_def
 
 
 class BaseApiTestClass:
     config_parser = configparser.ConfigParser()
-    TEST_DIR = os.path.dirname(os.path.abspath(__file__))
-    config_parser.read(TEST_DIR + '\\..\\..\\env\\config.ini')
-
+    config_parser.read(conf_def.CONF_DIR + '\\config.ini')
     domain = config_parser['env']['domain']
     api_token = config_parser['user']['api_token']
 
